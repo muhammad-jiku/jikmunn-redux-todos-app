67858f472bf7013b79aee760edf5f5a454f2b40c
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-17 21:19:55 +0600

    reducer added

---
+++ src/utils/reducers/index.js
@@ -0,0 +1,39 @@
+import {
+  GET_TODOS_FAILURE,
+  GET_TODOS_REQUEST,
+  GET_TODOS_SUCCESS,
+} from '../constants';
+
+//  todos initialstate
+const intialTodosState = {
+  todos: [],
+  isLoading: false,
+  error: null,
+};
+
+//  todos reducer
+export const todosReducer = (state = intialTodosState, action) => {
+  switch (action.type) {
+    case GET_TODOS_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case GET_TODOS_SUCCESS:
+      return {
+        ...state,
+        todos: action.payload,
+        isLoading: false,
+      };
+
+    case GET_TODOS_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
