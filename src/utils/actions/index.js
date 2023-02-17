c65f4ffb4aa52540ca9d61bed728b30c08809c94
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-17 21:53:58 +0600

    action modifies

--- src/utils/actions/index.js
+++ src/utils/actions/index.js
@@ -1,28 +1,45 @@
+import axios from 'axios';
 import {
   GET_TODOS_FAILURE,
   GET_TODOS_REQUEST,
   GET_TODOS_SUCCESS,
 } from '../constants';
 
+const API_URI = 'https://jsonplaceholder.typicode.com/todos';
+
 //  get todos request
-export const getTodosRequest = () => {
-  return {
+export const getTodosRequest = () => async (dispatch) => {
+  dispatch({
     type: GET_TODOS_REQUEST,
-  };
+  });
+
+  try {
+    const res = await axios.get(API_URI);
+    const todos = await res.data;
+    dispatch({
+      type: GET_TODOS_SUCCESS,
+      payload: todos,
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_TODOS_FAILURE,
+      payload: error.message,
+    });
+  }
 };
 
-//  get todos success
-export const getTodosSuccess = (todos) => {
-  return {
-    type: GET_TODOS_SUCCESS,
-    payload: todos,
-  };
-};
+// //  get todos success
+// export const getTodosSuccess = (todos) => async (dispatch) => {
+//   dispatch ({
+//     type: GET_TODOS_SUCCESS,
+//     payload: todos,
+//   });
+// };
 
-//  get todos failure
-export const getTodosFailure = (error) => {
-  return {
-    type: GET_TODOS_FAILURE,
-    payload: error,
-  };
-};
+// //  get todos failure
+// export const getTodosFailure = (error) => async (dispatch) => {
+//   dispatch ({
+//     type: GET_TODOS_FAILURE,
+//     payload: error,
+//   });
+// };
