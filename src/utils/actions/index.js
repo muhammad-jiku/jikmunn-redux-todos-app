7797bfe1663ea3582cffba6196348a2030db2e32
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-17 21:17:30 +0600

    actions added

--- src/utils/actions/index.js
+++ src/utils/actions/index.js
@@ -1,3 +1,28 @@
-export const GET_TODOS_REQUEST = 'GET_TODOS_REQUEST';
-export const GET_TODOS_SUCCESS = 'GET_TODOS_SUCCESS';
-export const GET_TODOS_FAILURE = 'GET_TODOS_FAILURE';
+import {
+  GET_TODOS_FAILURE,
+  GET_TODOS_REQUEST,
+  GET_TODOS_SUCCESS,
+} from '../constants';
+
+//  get todos request
+export const getTodosRequest = () => {
+  return {
+    type: GET_TODOS_REQUEST,
+  };
+};
+
+//  get todos success
+export const getTodosSuccess = (todos) => {
+  return {
+    type: GET_TODOS_SUCCESS,
+    payload: todos,
+  };
+};
+
+//  get todos failure
+export const getTodosFailure = (error) => {
+  return {
+    type: GET_TODOS_FAILURE,
+    payload: error,
+  };
+};
